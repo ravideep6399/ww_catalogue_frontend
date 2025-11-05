e9e77d79be45589360ad8505664abddc82355980
ravideep <ravideepsingh1000@gmail.com>
2025-11-06 01:36:34 +0530

    first commit for ww

---
+++ src/routes/mainRoutes.jsx
@@ -0,0 +1,9 @@
+import { Suspense, lazy } from "react";
+import { Route, Routes } from "react-router-dom";
+
+export default (
+    <Suspense>
+        <Routes>
+        </Routes>
+    </Suspense>
+);
